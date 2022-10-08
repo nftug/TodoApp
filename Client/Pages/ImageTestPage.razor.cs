8f7334b51c4a08492ddb26c6c80b36887ab696cb
nftug <whitepony1214@gmail.com>
2022-10-09 01:10:07 +0900

    Add ImageTestPage

---
+++ Client/Pages/ImageTestPage.razor.cs
@@ -0,0 +1,43 @@
+using Client.Shared.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace Client.Pages;
+
+public partial class ImageTestPage : ComponentBase
+{
+    private string? _imageBase64Source;
+    private IBrowserFile _fileToUpload;
+
+    private bool _isLoading;
+    private bool IsLoading
+    {
+        get => _isLoading;
+        set
+        {
+            if (value == _isLoading) return;
+            _isLoading = value;
+            StateHasChanged();
+        }
+    }
+
+    private const int MaxFileSize = 10 * 1024 * 1024;
+
+    private async Task OnSelectedFile(InputFileChangeEventArgs e)
+    {
+        if (!e.File.ContentType.StartsWith("image")) return;
+
+        _imageBase64Source = null;
+
+        using var stream = e.File.OpenReadStream(MaxFileSize);
+        _fileToUpload = e.File;
+
+        IsLoading = true;
+
+        var source = await stream.ConvertToBase64StringAsync();
+        _imageBase64Source = string.Format("data:image/png;base64,{0}", source);
+
+        IsLoading = false;
+    }
+}
